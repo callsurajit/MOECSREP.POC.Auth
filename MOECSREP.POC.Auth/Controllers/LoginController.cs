ac02dae86ff5c7dbf0e0ce7bf0f28a635e69fd18
Paul <PaulS3@michigan.gov>
2021-10-08 23:45:33 -0400

    Add project files.

---
+++ MOECSREP.POC.Auth/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MOECSREP.POC.Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MOECSREP.POC.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+                
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            JWTTokenGenerator jwtTokenGen = new JWTTokenGenerator(_config);
+
+            var user = jwtTokenGen.AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = jwtTokenGen.GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }        
+    }
+}
---
+++ MOECSREP.POC.Auth/JWTTokenGenerator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MOECSREP.POC.Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOECSREP.POC.Auth
+{
+    public class JWTTokenGenerator
+    {
+        private IConfiguration _config;
+
+        public JWTTokenGenerator(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var signCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims: new List<Claim>(), // claims (are used to filter the data)
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: signCredentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            // Validate the User Credentials using Azure AD
+            //if (login.Username == "admin")
+            //{
+                user = new UserModel { Username = "pauls3@michigan.gov", Password = "jeet123" };
+            //}
+            return user;
+        }
+    }
+}
